b1476ad5d606f5953ac274019e936f4595e4dc7c
Caleb Kivindyo Mutua <mutua.caleb@students.jkuat.ac.ke>
2021-11-03 00:18:14 +0300

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+   @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(post_params)
+
+    if @post.save 
+      redirect_to @post, notice: "Your post was successfully saved"
+    else
+      render 'new', notice: "Oh no, Caleb! I was unable to save your post."
+    end
+  end
+
+  def edit 
+  end
+
+  def  update
+    if @post.update post_params 
+      redirect_to @post, notice: "your article was successfully updated"
+    else 
+      render 'edit'
+    end
+  end
+
+  def destroy 
+    @post.destroy 
+    redirect_to posts_path
+  end
+
+private 
+  
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+end
